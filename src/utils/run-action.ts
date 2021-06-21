1d1d95f6605fd4c51df40542706e2e7f255f3011
Mat Jones <mat.jones@agilebits.com>
2021-06-21 15:52:05 -0400

    support global config files

--- src/utils/run-action.ts
+++ src/utils/run-action.ts
@@ -1,7 +1,14 @@
 import yargs from "yargs";
+import { Icons } from "./icons";
 
 export const runAction = (action: () => any) => {
   try {
+    if (process.env["YACLT_CONFIG_PATH"]) {
+      console.log(
+        `${Icons.info} Found configuration file at ${process.env["YACLT_CONFIG_PATH"]}`
+      );
+    }
+
     action();
   } catch (error) {
     if (error.message) {
