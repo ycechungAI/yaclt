cc46185dd9ca320eb20a04166e05434f80df40ae
Mat Jones <mat.jones@agilebits.com>
2021-06-22 17:37:18 -0400

    add required validationPattern argument for validate and prepare-release commands

--- src/cli/commands/validate.ts
+++ src/cli/commands/validate.ts
@@ -1,14 +1,27 @@
-import { Arguments, CommandModule } from "yargs";
+import { Arguments, CommandModule, Options } from "yargs";
 import { ActionValidate, ActionValidateOptions } from "../../actions/validate";
 import { runAction } from "../../utils/run-action";
 import { CliOptions, GlobalArgv } from "../options";
 
-export interface ValidateCommandOptions extends GlobalArgv {}
+export interface ValidateCommandOptions extends GlobalArgv {
+  validationPattern: string;
+}
+
+export const ValidationPatternOption: { [key: string]: Options } = {
+  e: {
+    alias: "validationPattern",
+    describe:
+      "A regular expression used to validate each individual changelog entry",
+    type: "string",
+    required: true,
+  },
+};
 
 export const ValidateCommand: CommandModule<{}, ValidateCommandOptions> = {
   command: "validate",
   describe: "Validate existing changelogs against the specified format",
   builder: {
+    ...ValidationPatternOption,
     ...CliOptions,
   },
   handler: (argv: Arguments<ValidateCommandOptions>) => {
@@ -16,7 +29,7 @@ export const ValidateCommand: CommandModule<{}, ValidateCommandOptions> = {
       logsDir: argv.logsDir,
       format: argv.format,
       changeTypes: argv.changeTypes,
-      requireIssueIds: argv.requireIssueIds,
+      validationPattern: argv.validationPattern,
     };
 
     runAction(() => ActionValidate(options));
--- src/utils/string-format.ts
+++ src/utils/string-format.ts
@@ -1,5 +1,32 @@
+import Handlebars from "handlebars";
+
 export const StringFormatParams = {
   changeType: "changeType",
+  changeTypes: "changeTypes",
   message: "message",
   issueId: "issueId",
+};
+
+export const formatToChangeTypeRegex = (format: string) => {
+  const changeTypeTemplatePattern = /\{\{\s*changeType\s*\}\}/;
+  const hasChangeType = changeTypeTemplatePattern.test(format);
+  const indexOfChangeType = format.search(changeTypeTemplatePattern);
+  const changeTypeHandlebars = hasChangeType
+    ? format.substring(
+        Math.max(0, indexOfChangeType - 2),
+        Math.min(
+          format.length,
+          indexOfChangeType +
+            StringFormatParams.changeType.length +
+            Math.max(
+              format
+                .slice(indexOfChangeType + StringFormatParams.changeType.length)
+                .indexOf("}}") + 3,
+              0
+            )
+        )
+      )
+    : "UNCATEGORIZED";
+  const changeTypeCompiledTemplate = Handlebars.compile(changeTypeHandlebars);
+  return changeTypeCompiledTemplate;
 };
