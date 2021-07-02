f607e2692c06259427f23e785e33b00d923a4db8
Mat Jones <mat.jones@agilebits.com>
2021-07-02 16:03:02 -0400

    remove releaseBranchPattern from config

--- yacltrc.js
+++ yacltrc.js
@@ -2,5 +2,4 @@ module.exports = {
   branchFormat: "[a-zA-Z]/([0-9]+)-.*",
   validationPattern: "\\[.*\\]\\s+.*\\s{#[\\d]+}",
   releaseNumber: () => require("./package.json").version.toString(),
-  releaseBranchPattern: "release-{{releaseNumber}}",
 };
