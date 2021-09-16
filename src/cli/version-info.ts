b55224d2d1e92b68b9e423cba54fbde6e7efcc7e
Mat Jones <mat@mjones.network>
2021-09-16 10:21:57 -0400

    move ascii logo to its own file

--- src/cli/version-info.ts
+++ src/cli/version-info.ts
@@ -1,25 +1,10 @@
 import chalk from "chalk";
+import { asciiLogo } from "./ascii-logo";
 
 // need to require this since it's outside the typescript project
 // eslint-disable-next-line unicorn/prefer-module, @typescript-eslint/no-var-requires
 const version = require("../../package.json").version;
 
-export const versionInfo = chalk.blueBright.bold(`
-                                                             /((((/     /(((((/
-                                                            /((((/     /(((((/
-                                                          (((((((     /(((((/
-                                                         ((((((((    /(((((/////
-           /((((((/       //(((((/          /((((((((   ((((((( //(((((((((((((/
- (((((/  /((((((((/    /((((((((((((    ((((((((((((/   ((((((/ /(//((((((/
- (((((/ ((((((((/   /(((((((((((((((   /((((((((/      /(((((/    /((((((/
- ((((/ /((((((/    /((((((/ \\(((((((  /((((((         /(((((/     /(((((/
-/(((/,/((((((/     /((((    /((((((/  /((((((        /((((((/    /(((((/
-\\(((((((((((/      /(((((((((((((((/  \\((((((((((((  /(((((/     /((((/
-   ((///(((/        \\(((((((// (((//   \\((((((((/   /(((((/      /(((/
-     /(((((/
-    /(((((/
-    (((((/
-     (///
-
-yaclt v${version}
-`);
+export const versionInfo = chalk.blueBright.bold(
+  `${asciiLogo}\nyaclt v${version}`
+);
